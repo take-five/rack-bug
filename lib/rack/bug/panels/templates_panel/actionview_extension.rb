99249cadc273f20ec09bee029720b71430d54d09
Tom Caspy <tom@sir.co.il>
2012-07-15 15:18:37 +0300

    making rack::bug work with rails 3

--- lib/rack/bug/panels/templates_panel/actionview_extension.rb
+++ lib/rack/bug/panels/templates_panel/actionview_extension.rb
@@ -1,12 +1,10 @@
 if defined?(ActionView) && defined?(ActionView::Template)
   ActionView::Template.class_eval do
-
-    def render_template_with_rack_bug(*args, &block)
-      Rack::Bug::TemplatesPanel.record(path_without_format_and_extension) do
-        render_template_without_rack_bug(*args, &block)
+    def render_with_rack_bug(*args, &block)
+      Rack::Bug::TemplatesPanel.record(virtual_path) do
+        render_without_rack_bug(*args, &block)
       end
     end
-
-    alias_method_chain :render_template, :rack_bug
+    alias_method_chain :render, :rack_bug
   end
 end
