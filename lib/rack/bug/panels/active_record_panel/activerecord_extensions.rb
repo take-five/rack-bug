0b7556ebb909e99073a7f5be732a0550edbdc089
Alexei Mikhailov <amikhailov83@gmail.com>
2012-08-06 19:53:51 +0600

    Rails 3 after_initialize style

--- lib/rack/bug/panels/active_record_panel/activerecord_extensions.rb
+++ lib/rack/bug/panels/active_record_panel/activerecord_extensions.rb
@@ -1,7 +1,14 @@
 if defined?(ActiveRecord)
   ActiveRecord::Base.class_eval do
+    # Rails 3 callback style
+    if respond_to?(:after_initialize)
+      after_initialize :after_initialize_with_rack_bug
 
-    if instance_methods.include?("after_initialize")
+      def after_initialize_with_rack_bug
+        Rack::Bug::ActiveRecordPanel.record(self.class.base_class.name)
+      end
+      private :after_initialize_with_rack_bug
+    elsif instance_methods.include?("after_initialize")
       def after_initialize_with_rack_bug
         Rack::Bug::ActiveRecordPanel.record(self.class.base_class.name)
         after_initialize_without_rack_bug
