669d423ff3c6a9c427d495f90842a6a3254953c7
Alexei Mikhailov <amikhailov83@gmail.com>
2012-08-15 17:31:42 +0600

    ActiveRecord panel: Rails 3.2 compatability

--- lib/rack/bug/panels/sql_panel/sql_extension.rb
+++ lib/rack/bug/panels/sql_panel/sql_extension.rb
@@ -1,8 +1,8 @@
 if defined?(ActiveRecord) &&  defined?(ActiveRecord::ConnectionAdapters)
   ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-    def log_with_rack_bug(sql, name, &block)
-      Rack::Bug::SQLPanel.record(sql, Kernel.caller) do
-        log_without_rack_bug(sql, name, &block)
+    def log_with_rack_bug(*args, &block)
+      Rack::Bug::SQLPanel.record(args.first, Kernel.caller) do
+        log_without_rack_bug(*args, &block)
       end
     end
 
